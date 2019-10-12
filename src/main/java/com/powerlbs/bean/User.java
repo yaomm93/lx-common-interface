ba747a7a7d92fadf6a86c9e515a74956b852a114
yaomm <yaomm93@163.com>
2019-10-12 16:27:13 +0800

    练习dubbo用的类和接口

---
+++ src/main/java/com/powerlbs/bean/User.java
@@ -0,0 +1,49 @@
+package com.powerlbs.bean;
+
+import java.io.Serializable;
+
+/**
+ * ClassName:${name}
+ * package:com.powerlbs.bean
+ *
+ * @date:2019/10/1 21:40
+ * @author:Yaoxiaoming
+ **/
+public class User implements Serializable {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/powerlbs/service/UserService.java
@@ -0,0 +1,16 @@
+package com.powerlbs.service;
+
+import com.powerlbs.bean.User;
+
+import java.util.List;
+
+/**
+ * ClassName:${name}
+ * package:com.powerlbs.service
+ *
+ * @date:2019/10/1 21:43
+ * @author:Yaoxiaoming
+ **/
+public interface UserService {
+    public List<User> selectUserInfo();
+}
